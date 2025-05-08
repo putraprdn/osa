b7d43feace9007a8375200ff48f64c4b3851952e
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-05-08 15:42:00 +0700

    Add submission for task 1.3

--- osa1/part1/src/App.jsx
+++ osa1/part1/src/App.jsx
@@ -2,7 +2,27 @@ const Header = ({ course }) => {
 	return <h1>{course}</h1>;
 };
 
-const Content = ({ section, task }) => {
+const Content = ({ parts = [] }) => {
+	console.log(parts);
+	return (
+		<div>
+			<Part
+				section={parts[0].name}
+				task={parts[0].exercises}
+			/>
+			<Part
+				section={parts[1].name}
+				task={parts[1].exercises}
+			/>
+			<Part
+				section={parts[2].name}
+				task={parts[2].exercises}
+			/>
+		</div>
+	);
+};
+
+const Part = ({ section, task }) => {
 	return (
 		<p>
 			{section} {task}
@@ -16,29 +36,26 @@ const Total = ({ total }) => {
 
 const App = () => {
 	const course = "Half Stack application development";
-	const part1 = "Fundamentals of React";
-	const exercises1 = 10;
-	const part2 = "Using props to pass data";
-	const exercises2 = 7;
-	const part3 = "State of a component";
-	const exercises3 = 14;
+	const part1 = {
+		name: "Fundamentals of React",
+		exercises: 10,
+	};
+	const part2 = {
+		name: "Using props to pass data",
+		exercises: 7,
+	};
+	const part3 = {
+		name: "State of a component",
+		exercises: 14,
+	};
 
 	return (
 		<div>
 			<Header course={course} />
-			<Content
-				section={part1}
-				task={exercises1}
+			<Content parts={[part1, part2, part3]} />
+			<Total
+				total={part1.exercises + part2.exercises + part3.exercises}
 			/>
-			<Content
-				section={part2}
-				task={exercises2}
-			/>
-			<Content
-				section={part3}
-				task={exercises3}
-			/>
-			<Total total={exercises1 + exercises2 + exercises3} />
 		</div>
 	);
 };
