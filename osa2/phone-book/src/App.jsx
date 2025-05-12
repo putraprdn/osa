9b85e75234e49fe52248298cfe15c06fd54af141
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-05-12 14:35:59 +0700

    Add submission for task 2.9 phone book step 4

--- osa2/phone-book/src/App.jsx
+++ osa2/phone-book/src/App.jsx
@@ -2,10 +2,15 @@ import { useState } from "react";
 
 const App = () => {
 	const [persons, setPersons] = useState([
-		{ name: "Arto Hellas", number: "030-1231231" },
+		{ name: "Arto Hellas", number: "040-123456" },
+		{ name: "Ada Lovelace", number: "39-44-5323523" },
+		{ name: "Dan Abramov", number: "12-43-234345" },
+		{ name: "Mary Poppendieck", number: "39-23-6423122" },
 	]);
 	const [newName, setNewName] = useState("");
 	const [newNumber, setNewNumber] = useState("");
+	const [filterValue, setFilterValue] = useState("");
+	const [filteredPersons, setFilteredPersons] = useState([]);
 
 	const handleChange = (e) => {
 		const value = e.target.value;
@@ -38,9 +43,32 @@ const App = () => {
 		setNewNumber("");
 	};
 
+	const handleFilter = (e) => {
+		const value = e.target.value;
+		setFilterValue(value);
+		if (value.length === 0 || !value) {
+			return setFilteredPersons([]);
+		}
+		const filteredArr = persons.filter((person) =>
+			person.name.toLowerCase().includes(value.toLowerCase())
+		);
+		setFilteredPersons(filteredArr);
+	};
+
+	const listToShow = filterValue ? filteredPersons : persons;
+
 	return (
 		<div>
 			<h2>Phonebook</h2>
+			<div>
+				filter shown with{" "}
+				<input
+					onChange={handleFilter}
+					value={filterValue}
+				/>
+			</div>
+
+			<h3>add a new</h3>
 			<form onSubmit={handleSubmit}>
 				<div>
 					name:{" "}
@@ -63,7 +91,7 @@ const App = () => {
 				</div>
 			</form>
 			<h2>Numbers</h2>
-			{persons.map((person) => (
+			{listToShow.map((person) => (
 				<p key={person.name}>
 					{person.name} {person.number}
 				</p>
