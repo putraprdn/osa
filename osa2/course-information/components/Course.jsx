116f6b62a77ea075283f2b34b1303db75a591123
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-05-12 10:02:14 +0700

    Add submission for task 2.4 & 2.5 course information

--- osa2/course-information/components/Course.jsx
+++ osa2/course-information/components/Course.jsx
@@ -1,18 +1,15 @@
 const Course = ({ course }) => {
-	const totalExec = course.parts.reduce(
-		(sum, part) => sum + part.exercises,
-		0
-	);
+	const total = course.parts.reduce((sum, part) => sum + part.exercises, 0);
 
 	return (
 		<div>
-			<h1>Half Stack application development</h1>
-			{course.parts.map(({ name, id, exercises }) => (
-				<p key={id}>
-					{name} {exercises}
+			<h1>{course.name}</h1>
+			{course.parts.map((part) => (
+				<p key={part.id}>
+					{part.name} {part.exercises}
 				</p>
 			))}
-			<p>total of {totalExec} exercises</p>
+			<strong>total of {total} exercises</strong>
 		</div>
 	);
 };
