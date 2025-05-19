0544cddefe1046aeb92131135db00642849fcbc2
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-05-19 12:27:30 +0700

    Add submission for task 4.2 Blog List step 2

---
+++ osa4/blog-list/app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { MONGODB_URL } = require("./utils/config");
+const logger = require("./utils/logger");
+const middleware = require("./utils/middleware");
+const blogRouter = require("./controllers/blog");
+
+const app = express();
+
+logger.info("connecting to", MONGODB_URL);
+
+mongoose
+	.connect(MONGODB_URL)
+	.then(() => {
+		logger.info("connected to MongoDB");
+	})
+	.catch((error) => {
+		logger.error("error connection to MongoDB:", error.message);
+	});
+
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(middleware.requestLogger);
+
+app.use("/api/blogs", blogRouter);
+
+app.use(middleware.unknownEndpoint);
+app.use(middleware.errorHandler);
+
+module.exports = app;
---
+++ osa4/blog-list/models/blog.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGO_URL;
+
+mongoose
+  .connect(url)
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+  const blogSchema = mongoose.Schema({
+	title: String,
+	author: String,
+	url: String,
+	likes: Number,
+});
+
+blogSchema.set("toJSON", {
+  transform: (_, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Blog", blogSchema);
