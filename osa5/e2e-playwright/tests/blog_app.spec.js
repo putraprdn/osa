1b2407fa566c2bb3e60cd86d1cdb2b7c3c0938e1
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-05-26 15:19:51 +0700

    Add submission for task 5.18 Blog List End To End Testing step 2

--- osa5/e2e-playwright/tests/blog_app.spec.js
+++ osa5/e2e-playwright/tests/blog_app.spec.js
@@ -1,12 +1,43 @@
 import { expect, test, describe, beforeEach } from "@playwright/test";
 
 describe("Blog app", () => {
-	beforeEach(async ({ page }) => {
+	beforeEach(async ({ page, request }) => {
+		await request.post("/api/testing/reset");
+		await request.post("/api/users", {
+			data: {
+				name: "Playwright",
+				username: "test",
+				password: "secret",
+			},
+		});
+
 		await page.goto("/");
 	});
 
 	test("Login form is shown", async ({ page }) => {
 		const locator = await page.getByText("log in to application");
 		await expect(locator).toBeVisible();
+	});
+
+	describe("Login", () => {
+		test("succeeds with correct credentials", async ({ page }) => {
+			await page.getByTestId("username").fill("test");
+			await page.getByTestId("password").fill("secret");
+			await page.getByRole("button", { name: "login" }).click();
+
+			const locator = await page.getByText("blogs");
+			await expect(locator).toBeVisible();
+		});
+
+		test("fails with wrong credentials", async ({ page }) => {
+			await page.getByTestId("username").fill("wrong");
+			await page.getByTestId("password").fill("wrong");
+			await page.getByRole("button", { name: "login" }).click();
+
+			const errorDiv = await page.locator(".error");
+			await expect(errorDiv).toContainText(
+				"invalid username or password"
+			);
+		});
 	});
 });
