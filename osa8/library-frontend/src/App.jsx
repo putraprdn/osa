e32a2616f3b778a3d11e52bfdd9612b7d34d8327
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-06-04 14:33:50 +0700

    Add submission for task 8.8 Authors view

--- osa8/library-frontend/src/App.jsx
+++ osa8/library-frontend/src/App.jsx
@@ -1,26 +1,53 @@
-import { useState } from "react";
+import { Routes, Route, Link } from "react-router-dom";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import { useQuery } from "@apollo/client";
+import { GET_ALL_AUTHORS } from "./queries";
 
 const App = () => {
-  const [page, setPage] = useState("authors");
+	const result = useQuery(GET_ALL_AUTHORS);
 
-  return (
-    <div>
-      <div>
-        <button onClick={() => setPage("authors")}>authors</button>
-        <button onClick={() => setPage("books")}>books</button>
-        <button onClick={() => setPage("add")}>add book</button>
-      </div>
+	if (result.loading) {
+		return <div>loading...</div>;
+	}
 
-      <Authors show={page === "authors"} />
+	return (
+		<div>
+			<div>
+				<Link to={"/"}>
+					<button>authors</button>
+				</Link>
+				<Link to={"/books"}>
+					<button>books</button>
+				</Link>
+				<Link to={"/books/create"}>
+					<button>add book</button>
+				</Link>
+			</div>
 
-      <Books show={page === "books"} />
+			{/* <Authors show={page === "authors"} />
 
-      <NewBook show={page === "add"} />
-    </div>
-  );
+			<Books show={page === "books"} />
+
+			<NewBook show={page === "add"} /> */}
+
+			<Routes>
+				<Route
+					path="/"
+					element={<Authors authors={result.data} />}
+				/>
+				<Route
+					path="/books"
+					element={<Books />}
+				/>
+				<Route
+					path="/books/create"
+					element={<NewBook />}
+				/>
+			</Routes>
+		</div>
+	);
 };
 
 export default App;
