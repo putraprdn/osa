f092e36cd273b465bae8f1e45b4f09e4a3845c90
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-06-13 10:14:46 +0700

    Add submission for task 9.17

--- osa9/flight-diary/backend/src/services/diaryService.ts
+++ osa9/flight-diary/backend/src/services/diaryService.ts
@@ -1,42 +1,30 @@
-import diaryData from '../../data/entries';
+import diaryData from "../../data/entries";
 
-import { 
-  NonSensitiveDiaryEntry, DiaryEntry, NewDiaryEntry
- } from '../types';
+import { DiaryEntry, NewDiaryEntry } from "../types";
 
 const diaries: DiaryEntry[] = diaryData;
 
 const getEntries = (): DiaryEntry[] => {
-  return diaries;
-};
-
-const getNonSensitiveEntries = (): NonSensitiveDiaryEntry[] => {
-  return diaries.map(({ id, date, weather, visibility }) => ({
-    id,
-    date,
-    weather,
-    visibility,
-  }));
+	return diaries;
 };
 
 const findById = (id: number): DiaryEntry | undefined => {
-  const entry = diaries.find(d => d.id === id);
-  return entry;
+	const entry = diaries.find((d) => d.id === id);
+	return entry;
 };
 
-const addDiary = ( entry: NewDiaryEntry ): DiaryEntry => {
-  const newDiaryEntry = {
-    id: Math.max(...diaries.map(d => d.id)) + 1,
-    ...entry
-  };
+const addDiary = (entry: NewDiaryEntry): DiaryEntry => {
+	const newDiaryEntry = {
+		id: Math.max(...diaries.map((d) => d.id)) + 1,
+		...entry,
+	};
 
-  diaries.push(newDiaryEntry);
-  return newDiaryEntry;
+	diaries.push(newDiaryEntry);
+	return newDiaryEntry;
 };
 
 export default {
-  getEntries,
-  addDiary,
-  getNonSensitiveEntries,
-  findById
+	getEntries,
+	addDiary,
+	findById,
 };
--- osa9/flight-diary/frontend/src/App.tsx
+++ osa9/flight-diary/frontend/src/App.tsx
@@ -1,6 +1,37 @@
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { BASE_URL } from "./constants";
+import type { DiaryEntry } from "./types";
+
 const App = () => {
-	const diaries: [] = [];
-	return <div>{diaries && diaries.map((d) => <p>{d}</p>)}</div>;
+	const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+
+	useEffect(() => {
+		const fetchData = async () => {
+			const response = await axios.get<DiaryEntry[]>(`${BASE_URL}/api/diaries`);
+			console.log(response.data)
+			setDiaries(response.data);
+		};
+		fetchData();
+	}, []);
+
+	return (
+		<>
+			<h1>Diary Entries</h1>
+			<div>
+				{diaries &&
+					diaries.map((d) => (
+						<div key={d.id}>
+							<p>
+								<strong>{d.date}</strong>
+							</p>
+							<div>visibility: {d.visibility}</div>
+							<div>weather: {d.weather}</div>
+						</div>
+					))}
+			</div>
+		</>
+	);
 };
 
 export default App;
