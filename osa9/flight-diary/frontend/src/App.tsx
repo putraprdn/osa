2966897dc4a2996dd74578f247e775798b76c957
putraprdn <80741806+putraprdn@users.noreply.github.com>
2025-06-13 10:29:30 +0700

    Add submission for task 9.18

--- osa9/flight-diary/frontend/src/App.tsx
+++ osa9/flight-diary/frontend/src/App.tsx
@@ -1,22 +1,96 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { BASE_URL } from "./constants";
-import type { DiaryEntry } from "./types";
+import {
+	type NewDiaryEntry,
+	Visibility,
+	Weather,
+	type DiaryEntry,
+} from "./types";
 
 const App = () => {
 	const [diaries, setDiaries] = useState<DiaryEntry[]>([]);
+	const [newEntryFields, setNewEntryFields] = useState<NewDiaryEntry>({
+		date: "",
+		visibility: Visibility.Good,
+		weather: Weather.Stormy,
+		comment: "",
+	});
 
 	useEffect(() => {
 		const fetchData = async () => {
-			const response = await axios.get<DiaryEntry[]>(`${BASE_URL}/api/diaries`);
-			console.log(response.data)
+			const response = await axios.get<DiaryEntry[]>(
+				`${BASE_URL}/api/diaries`
+			);
+			console.log(response.data);
 			setDiaries(response.data);
 		};
 		fetchData();
 	}, []);
 
+	const handleOnChange = (e: React.FormEvent<HTMLInputElement>) => {
+		setNewEntryFields({
+			...newEntryFields,
+			[e.currentTarget.name]: e.currentTarget.value,
+		});
+		console.log(newEntryFields);
+	};
+
+	const handleSubmit = async (e: React.SyntheticEvent) => {
+		e.preventDefault();
+		const response = await axios.post<DiaryEntry>(
+			`${BASE_URL}/api/diaries`,
+			newEntryFields
+		);
+		setDiaries(diaries.concat(response.data));
+		console.log(response.data);
+	};
+
 	return (
-		<>
+		<div>
+			<div>
+				<h2>Add new entry</h2>
+				<form onSubmit={handleSubmit}>
+					<div>
+						date{" "}
+						<input
+							onChange={handleOnChange}
+							value={newEntryFields.date}
+							type="text"
+							name="date"
+						/>
+					</div>
+					<div>
+						visibility{" "}
+						<input
+							onChange={handleOnChange}
+							value={newEntryFields.visibility}
+							type="text"
+							name="visibility"
+						/>
+					</div>
+					<div>
+						weather{" "}
+						<input
+							onChange={handleOnChange}
+							value={newEntryFields.weather}
+							type="text"
+							name="weather"
+						/>
+					</div>
+					<div>
+						comment{" "}
+						<input
+							onChange={handleOnChange}
+							value={newEntryFields.comment}
+							type="text"
+							name="comment"
+						/>
+					</div>
+					<button>add</button>
+				</form>
+			</div>
+
 			<h1>Diary Entries</h1>
 			<div>
 				{diaries &&
@@ -30,7 +104,7 @@ const App = () => {
 						</div>
 					))}
 			</div>
-		</>
+		</div>
 	);
 };
 
